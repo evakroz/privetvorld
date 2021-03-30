dd3f5de557e335f959509b6d7b7a3516214b0c08
Eva <eva.kroz.123@gmail.com>
2021-03-29 19:56:21 -0700

    min max

--- checkpoint_day_05/stock_prices.py
+++ checkpoint_day_05/stock_prices.py
@@ -1,2 +1,22 @@
+from random import *
 
-stock_price = []
+evas_tickers= []
+
+for var in range(5):
+    n = round(uniform(1500,3000), 2)
+    evas_tickers.append(n)
+
+print(evas_tickers)
+
+# smallest = min(evas_tickers)
+#
+# print({smallest})
+#
+# largest = max(evas_tickers)
+#
+# print({largest})
+
+# evas_tickers.sort(reverse=False)
+# print(evas_tickers)
+# print(evas_tickers[0])
+# print(evas_tickers[9])
